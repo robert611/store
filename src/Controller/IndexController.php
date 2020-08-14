9840e1ea2fdc7402242584d18725f4df9324a1db
robert611 <cyrkiel86@interia.pl>
2020-08-14 13:34:33 +0200

    Created product page template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
 
 class IndexController extends AbstractController
 {
@@ -14,6 +16,16 @@ class IndexController extends AbstractController
     public function index()
     {
         return $this->render('index/index.html.twig');
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show", methods={"GET"})
+     */
+    public function showProduct(Product $product): Response
+    {
+        return $this->render('index/show_product.html.twig', [
+            'product' => $product,
+        ]);
     }
 
     /**
