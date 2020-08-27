49d7f0968e3d0d3789142ef9848c224936a2f172
robert611 <cyrkiel86@interia.pl>
2020-08-27 18:14:29 +0200

    Added functionality uploading pictures from product form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,8 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Product;
+use App\Entity\ProductPicture;
 use App\Entity\DeliveryType;
 use App\Form\ProductType;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @IsGranted("ROLE_USER")
@@ -26,7 +28,7 @@ class AccountController extends AbstractController
     /**
      * @Route("/account/product/new", name="account_product_new")
      */
-    public function newProduct(Request $request)
+    public function newProduct(Request $request, SluggerInterface $slugger)
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
@@ -39,12 +41,46 @@ class AccountController extends AbstractController
 
             $deliveryTypeRepository = $this->getDoctrine()->getRepository(DeliveryType::class);
 
-            foreach ($deliveryTypes as $key => $type) 
+            foreach ($deliveryTypes as $type) 
             {
                 $entityManager->persist($deliveryTypeRepository->find($type)->addProduct($product));
             }
 
-            $product->addDeliveryType($this->getDoctrine()->getRepository(DeliveryType::class)->find(1));
+            $pictures = $form->get('pictures')->getData();
+     
+            /* Make sure there will not be more than 24 files */
+            if (count($pictures) > 24) {
+                $pictures = array_slice($pictures, 0, 24);
+            }
+
+            if ($pictures) {
+                foreach ($pictures as $picture) 
+                {
+                    $productPicture = new ProductPicture();
+
+                    $originalFilename = pathinfo($picture->getClientOriginalName(), PATHINFO_FILENAME);
+                    
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$picture->guessExtension();
+    
+                    try {
+                        $picture->move(
+                            $this->getParameter('pictures_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+
+                    $productPicture->setName($newFilename);
+                    $productPicture->setProduct($product);
+
+                    $product->addProductPicture($productPicture);
+
+                    $entityManager->persist($productPicture);
+                }
+            }
 
             $entityManager->persist($product);
             $entityManager->flush();
